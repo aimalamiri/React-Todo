8e695f0d7db31797b0df9f4c55c980249b6e5555
Aimal Amiri <meaimal@hotmail.com>
2022-05-30 20:07:09 +0430

    Enabled Strict mode

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/TodoContainer';
 
-ReactDOM.render(<TodoContainer />, document.querySelector('#root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>,
+  document.querySelector('#root')
+);
